bd15780783c6b0d7459e5de4fab4a4ccfddc1cf7
Ewen Le Hegarat <ewenlehegaratpro@gmail.com>
2026-02-24 10:10:59 +0100

    dark mode correction

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
 import { darkMode } from "./JS/darkmode.js"
 import { observe } from "./JS/observer.js"
+import { modifierCandidatures } from "./JS/modifierCandidatures.js"
 
 darkMode()
 observe()
-
-const modifierCandidatures = document.querySelector('.modifier_candidature')
-const buttonModifier = document.querySelectorAll('.button_modifier')
-const buttonFermerCandidatures = document.querySelector('.icon_close')
-
-buttonModifier.forEach(bm => {
-    bm.addEventListener('click', () => {
-        modifierCandidatures.style.display = 'flex'
-    })  
-});
-buttonFermerCandidatures.addEventListener('click', () => {
-    modifierCandidatures.style.display = 'none'
-})
+modifierCandidatures()
