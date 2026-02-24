3e526844000d194eb9eb4b2ad60704c8096df554
Ewen Le Hegarat <ewenlehegaratpro@gmail.com>
2026-02-24 11:22:02 +0100

    update

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import { darkMode } from "./JS/darkmode.js"
 import { observe } from "./JS/observer.js"
 import { modifierCandidatures } from "./JS/modifierCandidatures.js"
+import { afficherCandidatures } from "./JS/afficherCandidatures.js"
 
 darkMode()
 observe()
 modifierCandidatures()
+afficherCandidatures()
